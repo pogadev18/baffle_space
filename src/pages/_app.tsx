7438e6c4baacc4778859114df545d134528b36b1
Bogdan Pogacean <pogadev18@Bogdans-MacBook-Pro.local>
2022-07-14 16:57:27 +0300

    removed redux

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,14 +2,12 @@ import React from 'react';
 import { ChakraProvider } from '@chakra-ui/react';
 import { MoralisProvider } from 'react-moralis';
 import { NextPage } from 'next';
-import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 
 import AppLayout from '@/root/layouts/appLayout';
 import LandingPageLayout from '@/root/layouts/landingPageLayout';
 import AuthGuard from '@/root/components/authGuard';
 import { theme } from '@/root/theme';
-import { store } from '@/root/store/store';
 
 import 'reset-css';
 import 'react-toastify/dist/ReactToastify.css';
@@ -27,31 +25,29 @@ type AppProps = {
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
-    <Provider store={store}>
-      <ChakraProvider theme={theme}>
-        <MoralisProvider
-          appId={process.env.MORALIS_APP_ID!}
-          serverUrl={process.env.MORALIS_SERVER_URL!}
-        >
-          {Component.requireAuth ? (
-            <AuthGuard>
-              <AppLayout>
-                <Component {...pageProps} />
-              </AppLayout>
-            </AuthGuard>
-          ) : Component.landingPage ? (
-            <LandingPageLayout>
-              <Component {...pageProps} />
-            </LandingPageLayout>
-          ) : (
+    <ChakraProvider theme={theme}>
+      <MoralisProvider
+        appId={process.env.MORALIS_APP_ID!}
+        serverUrl={process.env.MORALIS_SERVER_URL!}
+      >
+        {Component.requireAuth ? (
+          <AuthGuard>
             <AppLayout>
               <Component {...pageProps} />
             </AppLayout>
-          )}
-          <ToastContainer />
-        </MoralisProvider>
-      </ChakraProvider>
-    </Provider>
+          </AuthGuard>
+        ) : Component.landingPage ? (
+          <LandingPageLayout>
+            <Component {...pageProps} />
+          </LandingPageLayout>
+        ) : (
+          <AppLayout>
+            <Component {...pageProps} />
+          </AppLayout>
+        )}
+        <ToastContainer />
+      </MoralisProvider>
+    </ChakraProvider>
   );
 };
 
