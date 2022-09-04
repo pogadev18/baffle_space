900e14d3685c60eba63db17d92ea0db6a3199de3
pogadev <pogadev18@gmail.com>
2022-09-04 17:46:53 +0300

    new hero plus mobile design

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -14,7 +14,7 @@ class MyDocument extends Document {
             rel="stylesheet"
           />
           <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700&display=swap"
             rel="stylesheet"
