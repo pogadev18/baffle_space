a0c704158e3b61dfc6b2324c3ff00ce2527ebc52
pogadev <pogadev18@gmail.com>
2022-10-09 15:59:48 +0300

    winnings text

--- src/components/landingPage/infoTexts/InfoTexts.tsx
+++ src/components/landingPage/infoTexts/InfoTexts.tsx
@@ -1,4 +1,4 @@
-import { ReactNode } from 'react';
+import { ReactNode, Fragment } from 'react';
 import { Box, Container, Heading, Text } from '@chakra-ui/react';
 
 import NftTree from '@/root/components/nftTree';
@@ -9,13 +9,20 @@ import Less365DaysGraph from '@/root/components/less365DaysGraph';
 import Exact365DaysGraph from '@/root/components/exact365DaysGraph';
 import SimulateEarnings from '@/root/components/landingPage/simulateEarnings';
 
-import { genesisNFTBenefits, less365DaysTexts, exact365DaysTexts } from '@/root/utils/texts';
+import {
+  genesisNFTBenefits,
+  less365DaysTexts,
+  exact365DaysTexts,
+  whatYouWinTexts,
+  winCategories,
+} from '@/root/utils/texts';
 
-// import styles from './InfoTexts.module.scss';
+import styles from './InfoTexts.module.scss';
 
 const textColor = '#fff';
 const titlesColor = '#00B0CA';
 const borderBlue = '#0070A2';
+const intervalColor = '#707070';
 
 export const SubHeading = ({ children }: { children: ReactNode }) => (
   <Heading
@@ -30,13 +37,19 @@ export const SubHeading = ({ children }: { children: ReactNode }) => (
   </Heading>
 );
 
-export const SoftHeading = ({ children }: { children: ReactNode }) => (
+export const SoftHeading = ({
+  children,
+  marginBottom,
+}: {
+  children: ReactNode;
+  marginBottom: string;
+}) => (
   <Heading
     as="h3"
     fontSize={{ base: '5vw', sm: '26px' }}
     fontWeight="400"
     color={titlesColor}
-    marginBottom="20px"
+    marginBottom={marginBottom}
     textTransform="uppercase"
   >
     {children}
@@ -63,12 +76,12 @@ export const FollowUs = ({ alignX }: { alignX: string }) => (
 const InfoTexts = () => {
   return (
     <Container
-      paddingY={{ base: '25px', md: '80px' }}
-      paddingX={{ base: '25px', md: '40px' }}
+      paddingY={{ base: '15px', md: '80px' }}
+      paddingX={{ base: '15px', md: '40px' }}
       maxW="8xl"
     >
       <Box marginBottom="60px" textAlign="center">
-        <SubHeading>What is it?</SubHeading>
+        <SubHeading>Why baffle space</SubHeading>
         <Text color={textColor} fontSize="16px" margin="auto" width={{ base: 'auto', md: '540px' }}>
           Dogecoin was few robust delegated proof-of-stake! NEO detected the algo-traded
           all-time-low at the ICO since Bitcoin Cash identified lots of fork, therefore, Waves
@@ -76,6 +89,62 @@ const InfoTexts = () => {
           Litecoin thought the Lambo in a nonce.
         </Text>
       </Box>
+
+      <Box
+        borderTop={`1px solid ${borderBlue}`}
+        marginBottom={{ base: '20px', md: '30px' }}
+        display="flex"
+        flexDirection={{ base: 'column', xl: 'row' }}
+        justifyContent="space-between"
+        paddingY={{ base: '30px', md: '30px' }}
+        paddingX={{ base: '15px', '2xl': '130px' }}
+        alignItems="center"
+        gap={{ base: '10px', xl: '60px' }}
+      >
+        <Box
+          width={{ base: '100%', xl: '50%' }}
+          marginTop={{ base: '0px', '2xl': '0px' }}
+          color="#fff"
+        >
+          <SubHeading>What you win</SubHeading>
+          <ul style={{ listStyle: 'disc', paddingLeft: '15px' }}>
+            {whatYouWinTexts.map((benefit) => (
+              <li key={benefit}>{benefit}</li>
+            ))}
+          </ul>
+        </Box>
+
+        <Box width={{ base: '100%', xl: '50%' }}>
+          <Box className={styles.winCategoriesPrizes}>
+            {winCategories.map((category) => {
+              const { dayInterval, info } = category;
+
+              return (
+                <Fragment key={dayInterval}>
+                  <Text
+                    className={styles.dayInterval}
+                    borderBottom={`1px solid ${intervalColor}`}
+                    textTransform="uppercase"
+                    fontSize="14px"
+                    color={intervalColor}
+                  >
+                    {dayInterval}
+                  </Text>
+                  {info.map((text) => (
+                    <Box className={styles.winDescription} key={text.subtitle}>
+                      <SoftHeading marginBottom="0px">{text.title}</SoftHeading>
+                      <Text fontSize="14px" color="#fff">
+                        {text.subtitle}
+                      </Text>
+                    </Box>
+                  ))}
+                </Fragment>
+              );
+            })}
+          </Box>
+        </Box>
+      </Box>
+
       <Box
         borderTop={`1px solid ${borderBlue}`}
         borderBottom={`1px solid ${borderBlue}`}
@@ -84,7 +153,7 @@ const InfoTexts = () => {
         flexDirection={{ base: 'column', '2xl': 'row' }}
         justifyContent="space-between"
         paddingY={{ base: '30px', md: '60px' }}
-        paddingX={{ base: '25px', '2xl': '130px' }}
+        paddingX={{ base: '15px', '2xl': '130px' }}
         alignItems="center"
       >
         <NftTree />
@@ -109,7 +178,7 @@ const InfoTexts = () => {
         borderBottom={`1px solid ${borderBlue}`}
         marginBottom={{ base: '20px', md: '60px' }}
         paddingBottom="30px"
-        paddingX={{ base: '25px', '2xl': '80px' }}
+        paddingX={{ base: '15px', '2xl': '80px' }}
       >
         <Box textAlign="center">
           <SubHeading>
@@ -142,12 +211,12 @@ const InfoTexts = () => {
           flexDirection={{ base: 'column', '2xl': 'row' }}
           justifyContent="space-between"
           paddingY="30px"
-          paddingX={{ base: '25px', '2xl': '80px' }}
+          paddingX={{ base: '15px', '2xl': '80px' }}
           gap="25px"
           alignItems="center"
         >
           <Box textAlign="left" color="#fff">
-            <SoftHeading>
+            <SoftHeading marginBottom="20px">
               Distribuția fondurilor din jocurile cu <br />
               durata mai mică de 365 de zile
             </SoftHeading>
@@ -165,12 +234,12 @@ const InfoTexts = () => {
           display="flex"
           flexDirection={{ base: 'column', '2xl': 'row' }}
           justifyContent="space-between"
-          paddingX={{ base: '25px', '2xl': '80px' }}
+          paddingX={{ base: '15px', '2xl': '80px' }}
           gap="25px"
           alignItems="center"
         >
           <Box textAlign="left" color="#fff">
-            <SoftHeading>
+            <SoftHeading marginBottom="20px">
               Distribuția fondurilor din jocurile cu <br /> durata de 365 de zile
             </SoftHeading>
             <ol style={{ listStyle: 'decimal', paddingLeft: '15px' }}>
--- src/utils/texts/index.ts
+++ src/utils/texts/index.ts
@@ -21,3 +21,54 @@ export const exact365DaysTexts = [
   'Development and maintenance revenue of Baffle Space',
   '10% din suma strânsă merge către un fond special de caritate, gestionat prin vot de către deținătorii de NFT-uri, indiferent de generație',
 ];
+
+export const whatYouWinTexts = [
+  'Sute de concursuri care rulează în paralel',
+  'Extrageri în fiecare zi',
+  'Sute de premii în fiecare categorie',
+  'Valoarea însumată a premiilor este de peste 1.000.000.000 $',
+  'Fiecare premiu reprezintă un joc și toate jocurile rulează în paralel, astfel vei putea participa în același timp la câte jocuri dorești, mărind șansele de câștig',
+  'Fiecare extragere garantează un câștigător',
+];
+
+export const winCategories = [
+  {
+    dayInterval: '7 days interval',
+    info: [
+      {
+        title: 'luxury electronics',
+        subtitle: 'Sute de modele în sute de concursuri care rulează în paralel',
+      },
+      {
+        title: 'luxury motors',
+        subtitle: 'Peste 100 de modele de motociclete din toate categoriile',
+      },
+    ],
+  },
+  {
+    dayInterval: '30 days interval',
+    info: [
+      {
+        title: 'luxury cars',
+        subtitle: 'Peste 200 de modele de mașini de lux',
+      },
+      {
+        title: 'luxury houses',
+        subtitle: 'Peste 100 de modele de motociclete din toate categoriile',
+      },
+      {
+        title: 'gold 999',
+        subtitle: 'Premii de la 100g până la 10kg de aur pur în lingouri',
+      },
+    ],
+  },
+  {
+    dayInterval: '90 days interval',
+    info: [
+      {
+        title: 'New Year Lottery',
+        subtitle: 'Premiu garantat de $1.000.000',
+      },
+    ],
+  },
+];
