8d1e81941df6454e9ce18ce857aff003d861f6f3
pogadev <pogadev18@gmail.com>
2022-10-10 11:53:04 +0300

    back to top link fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import BigLogo from '@/root/components/landingPage/bigLogo';
 import InfoTexts from '@/root/components/landingPage/infoTexts';
+import { Text } from '@chakra-ui/react';
 
 // import HomeSlider from '@/root/components/homeSlider';
 // import HomeWhaleSection from '@/root/components/HomeWhaleSection';
@@ -12,8 +13,13 @@ const Home = () => {
       <Head>
         <title>Baffle.space</title>
       </Head>
-      <BigLogo />
-      <InfoTexts />
+      <section id="#top">
+        <BigLogo />
+        <InfoTexts />
+      </section>
+      <Text marginBottom="20px" color="#00B0CA" fontWeight="700" textDecoration="underline">
+        <a href="#top">back to top</a>
+      </Text>
     </>
   );
 };
